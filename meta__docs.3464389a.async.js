10cb880b102da771bf1519711a36a651baaa8cd0
jinhuan138 <jinhuan138@users.noreply.github.com>
2026-01-28 05:43:02 +0000

    deploy: a264859ebec8b67732bb0f5db2e94dfca602d229

---
+++ meta__docs.3464389a.async.js
@@ -0,0 +1,12 @@
+"use strict";(self.webpackChunkreact_book_reader_docs=self.webpackChunkreact_book_reader_docs||[]).push([[904],{6312:function(l,r,e){e.r(r),e.d(r,{demos:function(){return v}});var a=e(29008),o=e.n(a),i=e(70958),m=e.n(i),d=e(92379),p=e(84296),v={"docs-guide-introduction-demo-0":{component:d.memo(d.lazy(m()(o()().mark(function _(){var u,s;return o()().wrap(function(n){for(;;)switch(n.prev=n.next){case 0:return n.next=2,Promise.resolve().then(e.bind(e,84296));case 2:return u=n.sent,s=u.ReactReader,n.abrupt("return",{default:function(){return d.createElement("div",{style:{height:"100vh"}},d.createElement(s,{url:"/files/\u557C\u7B11\u56E0\u7F18.epub"}))}});case 5:case"end":return n.stop()}},_)})))),asset:{type:"BLOCK",id:"docs-guide-introduction-demo-0",refAtomIds:[],dependencies:{"index.jsx":{type:"FILE",value:`import { ReactReader } from 'react-book-reader'
+
+export default () => {
+  return (
+    <div style={{ height: '100vh' }}>
+      {/* Supports EPUB, MOBI, KF8 (AZW3), FB2, CBZ, PDF */}
+      <ReactReader url="/files/\u557C\u7B11\u56E0\u7F18.epub" />
+    </div>
+  )
+}`},"react-book-reader":{type:"NPM",value:"1.0.1"}},entry:"index.jsx"},context:{"react-book-reader":p},renderOpts:{compile:function(){var _=m()(o()().mark(function s(){var c,n=arguments;return o()().wrap(function(t){for(;;)switch(t.prev=t.next){case 0:return t.next=2,e.e(195).then(e.bind(e,60195));case 2:return t.abrupt("return",(c=t.sent).default.apply(c,n));case 3:case"end":return t.stop()}},s)}));function u(){return _.apply(this,arguments)}return u}()}}}},5559:function(l,r,e){e.r(r),e.d(r,{demos:function(){return o}});var a=e(92379),o={}},60491:function(l,r,e){e.r(r),e.d(r,{texts:function(){return a}});const a=[{value:"an vue wrapper for ",paraId:0,tocIndex:0},{value:"foliate-js",paraId:0,tocIndex:0},{value:" - library for rendering e-books in the browser",paraId:0,tocIndex:0},{value:"::: code-group",paraId:1,tocIndex:1},{value:`npm install react-book-reader --save
+`,paraId:2,tocIndex:1},{value:`pnpm add  react-book-reader --save
+`,paraId:3,tocIndex:1},{value:":::",paraId:4,tocIndex:1},{value:"And in your react-component...",paraId:5,tocIndex:2}]},63256:function(l,r,e){e.r(r),e.d(r,{texts:function(){return a}});const a=[]}}]);
