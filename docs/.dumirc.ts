81d95058c89c08a2b3fb8372d0a705001d9e66f7
lindacong <2796009827@qq.com>
2026-01-28 11:26:13 +0800

    feat: Deploy

--- docs/.dumirc.ts
+++ docs/.dumirc.ts
@@ -2,11 +2,12 @@ import { defineConfig } from 'dumi';
 
 //https://d.umijs.org
 export default defineConfig({
+  base:'react-book-reader',
   outputPath: 'docs-dist',
   themeConfig: {
     name: 'react-book-reader',
     logo: "/logo.png",
-    nav: false
+    nav: [{ title: 'guide', link: '/introduction' }],
   },
   jsMinifierOptions: {
     target: 'es2020',
