f87f0c1f0534ad2213df583904d40fc4879ea22e
lindacong <2796009827@qq.com>
2026-02-04 10:25:06 +0800

    fix: docs package

---
+++ docs/.vitepress/theme/index.ts
@@ -0,0 +1,17 @@
+import DefaultTheme from 'vitepress/theme'
+import { EnhanceAppContext } from "vitepress"
+import pkg from '../../../package.json'
+import '../styles/index.scss'
+console.log(
+  `%c ${pkg.name} %c v`.concat(pkg.version, ' '),
+  'background: #f44336; padding: 1px; border-radius: 3px 0 0 3px; color: #fff',
+  'background: skyblue; padding: 1px; border-radius: 0 3px 3px 0; color: #fff',
+)
+
+export default {
+  ...DefaultTheme,
+  enhanceApp(ctx: EnhanceAppContext) {
+    DefaultTheme.enhanceApp(ctx)
+    const { app } = ctx
+  }
+}
