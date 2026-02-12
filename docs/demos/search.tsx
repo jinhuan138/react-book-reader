0bc4d546682bc0fcf47ef6596316b53950d7846e
lindacong <2796009827@qq.com>
2026-02-12 09:07:35 +0800

    fix: docs search demo

---
+++ docs/demos/search.tsx
@@ -0,0 +1,65 @@
+import { ReactReader } from 'react-book-reader'
+import { useState } from 'react'
+
+let rendition = null
+interface SearchItem {
+    label: string
+    cfi: string
+}
+export default function App() {
+
+    const [searchText, setSearchText] = useState('只在捻花一笑中')
+    const [searchResults, setSearchResults] = useState<SearchItem[]>([])
+
+    const search = async () => {
+        if (!searchText) return
+        const generator = await rendition.search({
+            scope: undefined,
+            query: searchText,
+            index: undefined,
+        })
+        const results = []
+        for await (const result of generator) {
+            if (typeof result === 'string') {
+                if (result === 'done') {
+                    console.log('search done')
+                }
+            } else {
+                if (result.progress) {
+                    console.log('search progress:', result.progress)
+                } else {
+                    results.push(result)
+                }
+            }
+        }
+        const tableResults: SearchItem[] = []
+        results.forEach(({ subitems }) => {
+            subitems.forEach((item) => {
+                const { pre, post } = item.excerpt
+                const label = `${pre}<span style='color: orange;'>${searchText}</span>${post}`
+                tableResults.push({
+                    label,
+                    cfi: item.cfi,
+                })
+            })
+        })
+        setSearchResults(tableResults)
+    }
+    return (
+        <div style={{ height: '100vh' }}>
+            <ReactReader url="/react-book-reader/files/啼笑因缘.mobi" getRendition={val => rendition = val} />
+            <div className="search">
+                <input placeholder="search" value={searchText} onChange={(e) => setSearchText(e.target.value)} onKeyDown={e => e.key === 'Enter' && search()} />
+                {!searchResults.length ? (
+                    <div>Empty</div>
+                ) : (
+                    searchResults.map(item => (
+                        <div key={item.cfi} onClick={() => rendition.goTo(item.cfi)}>
+                            <span dangerouslySetInnerHTML={{ __html: item.label }} ></span>
+                        </div>
+                    ))
+                )}
+            </div>
+        </div>
+    )
+}
